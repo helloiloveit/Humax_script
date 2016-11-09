76d56d7eee8b431b191dbfa23b8861200602cef7
huyheo <huyheo@huyheo-VirtualBox.(none)>
2016-11-09 13:49:02 +0700

    -first script for checking clean-build -1. Clean checkout -2. Makde defconfig -3. Build

---
+++ CMD/clean_build.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+print '...'
+import os
+import sys
+
+home_url = "/home/huyheo/work/Project/Test_New_Co"
+
+try:
+	target_file = sys.argv[1]
+except:
+	print 'wrong input, pls try again'
+	exit()
+print "### cd "+ home_url  
+#os.chdir("cd "+ home_url  )
+print "svn co http://svn.humaxdigital.com/drr_cdi/trunk/models/t51/ ./"
+#os.system("svn co http://svn.humaxdigital.com/drr_cdi/trunk/models/t51/ ./")
+
+print "### cd model/down"
+os.chdir("home_url + "/model/down")
+#os.chdir("/home/huyheo/work/Project/Test_New_Co/model/down")
+os.system("ls -l")
+print "### ./down.sh full_head"
+os.system("./down.sh full_head")
+
+print "### make t51_rev01_epg_defconfig"
+os.chdir(home_url +"/make")
+os.system("make t51_rev01_epg_defconfig")
+
+print "### make world"
+os.system("make world")
+
+print "### make image-mrs"
+os.system("make image-mrs")
